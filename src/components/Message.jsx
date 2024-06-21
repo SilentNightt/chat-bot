c9be0bc02d265aa76908b90bae3ad2ee4c7c263e
Stepanov Vadim <stepanov.v.b.00@mail.ru>
2024-06-21 17:35:20 +0300

    Конец верстки

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Tiger from '../pictures/tiger-jpg.jpg'
 
 const Message = () => {
   return (
-    <div className='message'>
-      message
+    <div className='message owner'>
+      <div className="message-info">
+        <img src={Tiger} alt=''/>
+        <span>recently</span>
+      </div>
+      <div className="meaasge-content">
+        <p>Hellow, how are u?</p>
+        <img src={Tiger} alt=''/>
+      </div>
     </div>
   )
 }
